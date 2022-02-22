2cd591787d8837f573d7eb947652fec6a469ccd1
Serhii Kupriienko <skupriienko@anaconda.com>
2022-02-22 10:09:31 +0000

    Remove exports

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,6 +1,3 @@
-export LDLIBS_TBB=$(echo $LDLIBS_TBB | sed 's/-Wl,--disable-new-dtags//g')
-export LDFLAGS_TBB=$(echo $LDFLAGS_TBB | sed 's/-Wl,--disable-new-dtags//g')
-
 echo $CMDSTAN
 
 cd $PREFIX/bin/cmdstan
