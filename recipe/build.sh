7665efae4c04e160efe3e9d94f5795839466f6ba
Serhii Kupriienko <skupriienko@anaconda.com>
2023-04-27 12:27:29 +0300

    Debug build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,9 @@ cp -r . $PREFIX/bin/cmdstan
 
 cd $PREFIX/bin/cmdstan
 
+ls -lia ./make/local
+ls -lia ./make/local.example
+
 echo "TBB_CXX_TYPE=${c_compiler}"  >> make/local
 echo "TBB_INTERFACE_NEW=true" >> make/local
 echo "TBB_INC=${PREFIX}/include/" >> make/local
