329602a124d2460c7a47d4eef02e0500b21b5dda
Serhii Kupriienko <skupriienko@anaconda.com>
2023-04-27 12:31:43 +0300

    Debug build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,8 +10,11 @@ cp -r . $PREFIX/bin/cmdstan
 
 cd $PREFIX/bin/cmdstan
 
-ls -lia ./make/local
-ls -lia ./make/local.example
+if [ "${target_platform}" == 'linux-s390x' ]; then
+    ls -lia ./make/local.example
+    cp -R ./make/local.example ./make/local
+    ls -lia ./make/local
+fi
 
 echo "TBB_CXX_TYPE=${c_compiler}"  >> make/local
 echo "TBB_INTERFACE_NEW=true" >> make/local
